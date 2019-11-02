4ac4093b3ade93dec314c2f5b20057f9485848c5
MC <dhy04057@likelion.org>
2019-11-02 15:25:49 +0900

    Updates

---
+++ precache-manifest.b126e54757ef8400d3d45f7179c758e9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6bc3dbdab6ec541027a5522c52406597",
+    "url": "/movie_app_2019/index.html"
+  },
+  {
+    "revision": "0c853233f794b1495089",
+    "url": "/movie_app_2019/static/css/main.51020db5.chunk.css"
+  },
+  {
+    "revision": "cbc22d1e9f5e8712be39",
+    "url": "/movie_app_2019/static/js/2.076e7f06.chunk.js"
+  },
+  {
+    "revision": "0c853233f794b1495089",
+    "url": "/movie_app_2019/static/js/main.7cbb8762.chunk.js"
+  },
+  {
+    "revision": "ad5f6e53801371c42eda",
+    "url": "/movie_app_2019/static/js/runtime-main.d36414e8.js"
+  }
+]);
